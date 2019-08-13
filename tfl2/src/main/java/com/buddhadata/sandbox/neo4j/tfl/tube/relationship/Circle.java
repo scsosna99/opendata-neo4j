13f580971879b931d57d4a1bbaf1ee297c6a279d
Scott C Sosna <me@scottsosna.dev>
2019-08-12 21:50:56 -0500

    segment-based TFL tube graph instead of putting labels on the stops

---
+++ tfl2/src/main/java/com/buddhadata/sandbox/neo4j/tfl/tube/relationship/Circle.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2018  Scott C. Sosna  ALL RIGHTS RESERVED
+ */
+
+package com.buddhadata.sandbox.neo4j.tfl.tube.relationship;
+
+import com.buddhadata.sandbox.neo4j.tfl.tube.enums.Direction;
+import com.buddhadata.sandbox.neo4j.tfl.tube.node.StopNode;
+import org.neo4j.ogm.annotation.RelationshipEntity;
+
+/**
+ * Relationship representing a segment/direct connection between two stops of a TfL route, useful for all modes.
+ *
+ * @author Scott C Sosna
+ */
+@RelationshipEntity(type = "CIRCLE")
+public class Circle extends RouteSegmentBase {
+
+    /**
+     * Constructor
+     */
+    public Circle() {
+    }
+
+    /**
+     * Constructor
+     * @param start starting stop (node) of this segment
+     * @param end ending stop (node) of this segment
+     * @param direction direction of the segment
+     * @param distance distance in meters between the two stops
+     */
+    public Circle(StopNode start,
+                  StopNode end,
+                  Direction direction,
+                  Double distance) {
+        super (start, end, direction, distance);
+    }
+}
